c9bcece2a722809833de0d092ac2949aedfe4bbd
Mxmilu666 <mxmilu666@gmail.com>
2026-03-30 22:04:03 +0800

    feat(version): update version to 0.0.4

--- backend/version/version.go
+++ backend/version/version.go
@@ -9,7 +9,7 @@ import (
 // These variables are expected to be overridden by -ldflags during build.
 var (
 	AppName   = "LoliaShizuku"
-	Version   = "0.0.3"
+	Version   = "0.0.4"
 	GitCommit = "dev"
 	GitBranch = "dev"
 	BuildTime = "unknown"
